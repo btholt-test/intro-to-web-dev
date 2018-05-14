2077e194457e277562c5579904759f4da060b7b4
Brian Holt <btholt@gmail.com>
2018-05-13 18:03:47 -0700

    Initial commit

---
+++ public/complements.js
@@ -0,0 +1,14 @@
+document
+  .querySelector(".request-complement")
+  .addEventListener("click", function() {
+    fetch("/complement")
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        document.querySelector(".complement").innerText = data.complement;
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+  });
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const complements = [
+  "You like nice today",
+  "That dress looks nice on you",
+  "Have you been working out?",
+  "You can do hard things",
+  "You've gotten far in this course. You're really smart",
+  "You're programming! How cool is that?",
+  "I'm really proud of you",
+  "You made this",
+  "You've learned a lot of things, and that's pretty hard to do"
+];
+
+function getRandomComplement() {
+  const randomIndex = Math.floor(Math.random() * complements.length);
+  return complements[randomIndex];
+}
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/complement", function(req, res) {
+  res
+    .json({
+      complement: getRandomComplement()
+    })
+    .end();
+});
+
+app.use("/public", express.static("./public"));
+
+app.listen(3000);
+console.log("listening on http://localhost:3000");
